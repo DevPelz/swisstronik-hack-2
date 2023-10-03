5cc79e61ee843545ec39ff358fd51fff0bbdf6cd
Dev_Pelz <pelzfx1@gmail.com>
2023-10-03 19:01:04 +0100

    modified: hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { configDotenv } from "dotenv";
+require("dotenv").config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC,
+      //@ts-ignore
+      accounts: [process.env.PRIVATEKEY],
+    },
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/", //URL of the RPC node for Swisstronik.
+      //@ts-ignore
+      accounts: [process.env.PRIVATEKEY], //Your private key starting with "0x"
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
